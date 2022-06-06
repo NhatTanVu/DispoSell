47161cb397fd86b27b218638738fae08a91edc44
Nhat Tan Vu <onchua2006@gmail.com>
2022-06-05 21:46:55 -0700

    Add responsive CSS styles + Refactor to header and footer component

--- code/src/main/js/App.js
+++ code/src/main/js/App.js
@@ -8,30 +8,25 @@ import '../scss/App.scss';
 import Home from './pages/Home';
 import Category from './pages/Category';
 import About from './pages/About';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import {Container, Nav, Navbar} from "react-bootstrap";
 
 export default function App() {
     return (
         <Router>
             <div className="App">
-                <Navbar bg="dark" variant="dark">
+                <Header/>
+                <main>
                     <Container>
-                        <Nav className="me-auto">
-                            <Navbar.Brand href="/">DispoSell</Navbar.Brand>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/category">Category</Nav.Link>
-                            <Nav.Link href="/about">About</Nav.Link>
-                        </Nav>
+                        <Routes>
+                            <Route path="/about" element={<About/>}/>
+                            <Route path="/category" element={<Category/>}/>
+                            <Route path="/" element={<Home/>}/>
+                        </Routes>
                     </Container>
-                </Navbar>
-
-                <Container>
-                    <Routes>
-                        <Route path="/about" element={<About/>}/>
-                        <Route path="/category" element={<Category/>}/>
-                        <Route path="/" element={<Home/>}/>
-                    </Routes>
-                </Container>
+                </main>
+                <Footer/>
             </div>
         </Router>
     );
---
+++ code/src/main/js/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+    return(
+        <footer className="py-2 bg-dark fixed-bottom">
+            <p className="m-0 text-center text-white">
+                Copyright &copy; DispoSell 2022
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
