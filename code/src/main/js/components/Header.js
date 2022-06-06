449bb1569dbf006d9945c408ac78eac7abf35f28
Nhat Tan Vu <onchua2006@gmail.com>
2022-06-05 22:33:45 -0700

    Change navigation layout

--- code/src/main/js/components/Header.js
+++ code/src/main/js/components/Header.js
@@ -4,13 +4,18 @@ import React from "react";
 function Header() {
     return (
         <header>
-            <Navbar bg="dark" variant="dark">
-                <Nav className="me-auto ps-2">
+            <Navbar bg="dark" variant="dark" expand="csm">
+                <Container>
                     <Navbar.Brand href="/">DispoSell</Navbar.Brand>
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/category">Category</Nav.Link>
-                    <Nav.Link href="/about">About</Nav.Link>
-                </Nav>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav>
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/category">Category</Nav.Link>
+                            <Nav.Link href="/about">About</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
             </Navbar>
         </header>
     )
