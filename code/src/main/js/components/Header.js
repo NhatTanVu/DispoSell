18a9bc285d480baf88d49fdc780f47bdc66ed8bf
Nhat Tan Vu <onchua2006@gmail.com>
2022-06-12 19:15:22 -0700

    DIS-49 Replace <a> tag with React Router Link

--- code/src/main/js/components/Header.js
+++ code/src/main/js/components/Header.js
@@ -1,18 +1,19 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 import React from "react";
+import {Link} from "react-router-dom";
 
 function Header() {
     return (
         <header>
             <Navbar bg="dark" variant="dark" expand="sm">
                 <Container>
-                    <Navbar.Brand href="/">DispoSell</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">DispoSell</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                     <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
                         <Nav>
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/category">Category</Nav.Link>
-                            <Nav.Link href="/leadership">Leadership</Nav.Link>
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/category">Category</Nav.Link>
+                            <Nav.Link as={Link} to="/leadership">Leadership</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
