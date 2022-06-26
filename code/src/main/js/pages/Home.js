bc6e56fef554532b51ce58575856ea655db9a775
Khang-Truong <khangtruong1995@gmail.com>
2022-06-26 16:56:45 -0700

    DIS-61_create_product_component

--- code/src/main/js/pages/Home.js
+++ code/src/main/js/pages/Home.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import localStyles from '../../scss/pages/Home.module.scss';
+import Product from "../components/Product";
 
 function Home() {
     return (
         <div className={localStyles["home-page"]}>
             <h2>Home</h2>
+            {/* add component for testing  */}
+            <Product/>
         </div>
     )
 }
