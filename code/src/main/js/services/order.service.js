c9d7f1e0f10759ad9200d97a4c4feec816fb82ca
ferryalvianto <ferryalvianto@gmail.com>
2022-07-12 11:41:39 -0700

    commit 2

---
+++ code/src/main/js/services/order.service.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import authHeader from './auth-header';
+
+class OrderService {
+    createPurchaseOrder(purchaseOrder) {
+        return axios.post("./api/createpurchaseorder",
+            purchaseOrder);
+    }
+
+    createTradeOrder(tradeOrder) {
+        return axios.post("./api/createtradeorder", {
+            tradeOrder
+        }, {
+            headers: authHeader()
+        });
+    }
+}
+
+export default new OrderService();
