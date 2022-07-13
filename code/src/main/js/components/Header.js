a4c5ffe096f064fe27a17c18e7e1f17b18f2746f
ferryalvianto <ferryalvianto@gmail.com>
2022-07-12 22:47:22 -0700

    commmit 3

--- code/src/main/js/components/Header.js
+++ code/src/main/js/components/Header.js
@@ -77,7 +77,7 @@ function Header() {
                                     </>)}
                                 </NavDropdown>
                                 &nbsp;
-                                <Nav.Link as={Link} to="/cart_payment">Cart</Nav.Link>
+                                <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
                                 &nbsp;
                                 <Nav.Link as={Link} to="/trade">
                                     <button className={`rounded-pill ${localStyles["btnTrade"]}`}>Trade
