09c527a3f4f54668782aaa396c2f08c71bcf6a17
mariana-mdv <menesesdelvallem@student.douglascollege.ca>
2022-07-14 15:25:29 -0700

    DIS-7 - API for ProductDetails

--- code/src/main/java/DispoSell/controllers/ProductController.java
+++ code/src/main/java/DispoSell/controllers/ProductController.java
@@ -2,8 +2,10 @@ package DispoSell.controllers;
 
 import DispoSell.models.*;
 import DispoSell.repositories.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
@@ -39,4 +41,21 @@ public class ProductController {
         List<Product> list = productRepository.findAll();
         return list;
     }
+
+    @GetMapping("/api/productdetail/id=")
+    public String getProductByID(Model model, @RequestParam(name="productID", defaultValue = "")
+    String product) {
+        List<Product> products;
+
+        if(product.isEmpty()){
+            products = productRepository.findAll();
+
+        } else {
+            long id = Long.parseLong(product);
+            products = productRepository.findByProductID(id);
+        }
+        model.addAttribute("productId", products);
+        return "products";
+    }
+
 }
--- code/src/main/java/DispoSell/repositories/ProductRepository.java
+++ code/src/main/java/DispoSell/repositories/ProductRepository.java
@@ -4,9 +4,12 @@ import DispoSell.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findByName(String name);
+
+    List<Product> findByProductID(Long productID);
 }
