d8abeaaf39241ae9f46edcf5f6511c4ae9ba8b5b
mariana-mdv <menesesdelvallem@student.douglascollege.ca>
2022-07-25 11:35:52 -0700

    DIS-9-ScheduleDelivery-API for List of Shippers & API for PurchaseOrder and TraderOrder By ID

--- code/src/main/java/DispoSell/repositories/PurchaseOrderRepository.java
+++ code/src/main/java/DispoSell/repositories/PurchaseOrderRepository.java
@@ -1,9 +1,11 @@
 package DispoSell.repositories;
 
+import DispoSell.models.Order;
 import DispoSell.models.PurchaseOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PurchaseOrderRepository extends JpaRepository<PurchaseOrder, Long> {
+    Order findByOrderID(Long orderID);
 }
