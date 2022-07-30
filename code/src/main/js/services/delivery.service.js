25a23d83a4d4a10898dbc9ba591c0e535ccf307c
Nhat Tan Vu <onchua2006@gmail.com>
2022-07-29 19:52:42 -0700

    DIS-9 Fix issue when calling createScheduleDelivery and getShippers methods

--- code/src/main/js/services/delivery.service.js
+++ code/src/main/js/services/delivery.service.js
@@ -1,15 +1,22 @@
 import axios from "axios";
 import authHeader from './auth-header';
 
-const API_URL = './api/auth/shippers';
-
 class DeliveryService {
     createScheduleDelivery(order) {
-        return axios.post('./api/scheduleDelivery', {order}, {headers: authHeader()});
+        return axios.post('/api/scheduleDelivery', {
+            "orderID": 3,
+            "vehicleNumber": "12345",
+            "vehicleType": "van",
+            "startTime": "2022-07-26T10:54:00-07:00",
+            "endTime": "2022-07-26T15:54:00-07:00",
+            "startLocation": "from somewhere",
+            "endLocation": "to somewhere",
+            "shippers": [3]
+        }, {headers: authHeader()});
     }
 
     getShippers() {
-        return axios.get(API_URL + 'admin' , { headers: authHeader() });
+        return axios.get("/api/auth/shippers", {headers: authHeader()});
     }
 }
 
